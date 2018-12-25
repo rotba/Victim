2f2fdb367e06bc0dfcd2a67ef7a9274c42d1cd68
Rotem <rotba@post.bgu.ac.il>
2018-12-25 17:11:22 +0200

    nop

--- Victim/Victim/Program.cs
+++ Victim/Victim/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,20 @@ namespace Victim
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(localEndPoint);
+            socket.Listen(10);
+            Socket client = socket.Accept();
+            while (client.Connected)
+            {
+                //Read the command's Type.
+                byte[] buffer = new byte[4];
+                int readBytes = client.Receive(buffer, 4, SocketFlags.None);
+                if (readBytes == 0)
+                    break;
+                Console.WriteLine(System.Text.Encoding.Default.GetString(buffer));
+            }
         }
     }
 }
---
+++ Victim/Victim/VictimServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victim
+{
+    class VictimServer
+    {
+        private int _listenPort;
+        public VictimServer(int listenPort) {
+            _listenPort = listenPort;
+        }
+        public void serve() {
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(localEndPoint);
+            socket.Listen(10);
+            Socket client = socket.Accept();
+            while (true)
+            {
+                handleClinet()
+            }
+        }
+    }
+}
